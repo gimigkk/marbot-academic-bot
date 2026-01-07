07d75cf1b866fdd50e62199dfb73ac8b33b5fa07
Arya Putra <aryafaiz108@gmail.com>
2026-01-07 20:24:45 +0700

    adding remainder h-1 jam

--- backend/migrations/20251221142621_init_schema.down.sql
+++ backend/migrations/20251221142621_init_schema.down.sql
@@ -1,6 +1,14 @@
+-- Hapus Policy
+DROP POLICY IF EXISTS "Enable access to all users" ON public.user_completions;
+DROP POLICY IF EXISTS "Enable access to all users" ON public.wa_logs;
+DROP POLICY IF EXISTS "Enable access to all users" ON public.assignments;
+DROP POLICY IF EXISTS "Enable access to all users" ON public.courses;
 
+-- Hapus Tabel 
+DROP TABLE IF EXISTS public.user_completions;
 DROP TABLE IF EXISTS public.wa_logs;
-
 DROP TABLE IF EXISTS public.assignments;
-
 DROP TABLE IF EXISTS public.courses;
+
+-- Hapus Extension
+DROP EXTENSION IF EXISTS "uuid-ossp";
