4d60773045f67515a266c8f08bbd20b519fb6cd4
Arya Putra <aryafaiz108@gmail.com>
2026-05-13 14:57:19 +0700

    fix

--- backend/src/pi/handler.rs
+++ backend/src/pi/handler.rs
@@ -584,6 +584,10 @@ pub async fn check_urgent_pi_deadlines(pool: &PgPool, logger: &JobLogger) -> Res
             .map_err(|e| e.to_string())?;
     }
 
+
+
+
+    
     logger.log("✅ Reminder urgent PI berhasil dikirim.");
     Ok(())
 }
